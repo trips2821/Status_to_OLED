c471b72373d692ed3f4d0f730d4718bec8fc2eed
Matthew Johnson <trips2821@gmail.com>
2016-05-02 21:53:32 -0600

    added ssid, hostname

--- update_status_oled.py
+++ update_status_oled.py
@@ -1,6 +1,17 @@
 import serial
 import time
 import socket
+from subprocess import Popen,PIPE
+import re, dbus, gtk, glib, sys, os
+ 
+def checkCurrentSsid():
+    ssidpat = re.compile('ESSID:"([^"]*)"')
+    iwconf = Popen(['iwconfig'], stdout=PIPE).communicate()[0]
+    if ssidpat.search(iwconf):
+        ssid = ssidpat.search(iwconf).groups()[0]
+        print "ssid ....... " +ssid
+        
+	return ssid
 
 def getNetworkIp():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -8,13 +19,14 @@ def getNetworkIp():
     return s.getsockname()[0]
 
 def main():
-    s = serial.Serial('COM14', 115200, timeout=.1)
+    s = serial.Serial('/dev/ttyACM0', 115200, timeout=.1)
     time.sleep(2)
 
     while True:
         ip_string = "IP:" + getNetworkIp()
         hostname_string = socket.gethostname()
-        s.write(hostname_string + '\\' + ip_string)
+        ssid = checkCurrentSsid()
+        s.write(ssid + '\\' + hostname_string + '\\' + ip_string)
         time.sleep(1)
         print s.read(3)
 
